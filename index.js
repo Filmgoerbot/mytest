db4423db7c858e2aa5ef7380ca69a509dfa84e30
Albin N J <albinnj0214@gmail.com>
2023-03-08 15:26:42 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const dotenv=require('dotenv');
+
+const app = express();
+dotenv.config()
+
+app.use('/',(req,res)=>{
+    res.json("Hi it is for test")
+})
+
+app.listen(process.env.PORT || 5000,()=>{
+    console.log("Server Started");
+})
