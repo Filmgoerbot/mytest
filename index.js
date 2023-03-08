1f8f9d07308ba351df44ace70e5534a2e0bf99ec
Albin N J <albinnj0214@gmail.com>
2023-03-08 16:51:21 +0530

    Changed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 dotenv.config()
 
 app.use('/',(req,res)=>{
-    res.json("Hi it is for test...")
+    res.json("Hi it is me Albin")
 })
 
 app.listen(process.env.PORT || 5000,()=>{
